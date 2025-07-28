7be6dc687bf3c9b2d89913053d9cba2b9974020b
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-07-28 17:17:20 -0600

    feat: add sys property for local run configuration

--- src/test/java/io/github/santiago120600/resources/Utils.java
+++ src/test/java/io/github/santiago120600/resources/Utils.java
@@ -7,10 +7,12 @@ import java.util.Properties;
 public class Utils {
 
     public static String getGlobalValue(String key) throws IOException {
+        String env = System.getProperty("config", "local");
+        String propFile = env.equals("local") ? "global-local.properties" : "global.properties";
         Properties properties = new Properties();
-        try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream("global.properties")) {
+        try (InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(propFile)) {
             if (inputStream == null) {
-                throw new IOException("global.properties file not found in classpath");
+                throw new IOException(propFile + " file not found in classpath");
             }
             properties.load(inputStream);
         }
